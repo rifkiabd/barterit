368306492b2a1120c1c4a8617e7bfbf6d12300ae
rifkiabd <110566354+rifkiabd@users.noreply.github.com>
2023-06-13 11:31:48 +0800

    Add files via upload

--- database/barterit/barterit_db.sql
+++ database/barterit/barterit_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 20, 2023 at 11:03 AM
+-- Generation Time: Jun 13, 2023 at 03:31 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `barterit_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_catches`
+--
+
+CREATE TABLE `tbl_catches` (
+  `catch_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `catch_name` varchar(255) DEFAULT NULL,
+  `catch_type` varchar(255) DEFAULT NULL,
+  `catch_desc` varchar(255) DEFAULT NULL,
+  `catch_price` varchar(255) DEFAULT NULL,
+  `catch_qty` varchar(255) DEFAULT NULL,
+  `catch_lat` varchar(255) DEFAULT NULL,
+  `catch_long` varchar(255) DEFAULT NULL,
+  `catch_state` varchar(255) DEFAULT NULL,
+  `catch_locality` varchar(255) DEFAULT NULL,
+  `catch_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_catches`
+--
+
+INSERT INTO `tbl_catches` (`catch_id`, `user_id`, `catch_name`, `catch_type`, `catch_desc`, `catch_price`, `catch_qty`, `catch_lat`, `catch_long`, `catch_state`, `catch_locality`, `catch_date`) VALUES
+(1, 4, 'laptop', 'Electronics', 'good', '122', '1', '37.421998333333335', '-122.084', 'California', 'Mountain View', '2023-06-13 01:46:36'),
+(2, 1, 'New Laptop', 'Electronics', 'New Laptop Good Condition', '1000', '1', '37.421998333333335', '-122.084', 'California', 'Mountain View', '2023-06-13 02:32:20'),
+(3, 1, 'Smartphone', 'Electronics', 'New Smartphone', '1000', '10', '37.421998333333335', '-122.084', 'California', 'Mountain View', '2023-06-13 02:36:10'),
+(4, 1, 'Bicyle', 'Equipment', 'Used Bicyle', '500', '1', '37.421998333333335', '-122.084', 'California', 'Mountain View', '2023-06-13 02:49:26');
 
 -- --------------------------------------------------------
 
@@ -43,11 +74,20 @@ CREATE TABLE `tbl_users` (
 
 INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_name`, `user_phone`, `user_password`, `user_otp`, `user_datereg`) VALUES
 (1, 'abdillah@gmail.com', 'Abdillah M Rifki', '01232132133', '601f1889667efaebb33b8c12572835da3f027f78', '64358', '2023-05-20 03:57:33'),
-(2, 'rozak@gmail.com', 'M Rozak', '012313223112', '601f1889667efaebb33b8c12572835da3f027f78', '13146', '2023-05-20 10:50:26');
+(2, 'rozak@gmail.com', 'M Rozak', '012313223112', '601f1889667efaebb33b8c12572835da3f027f78', '13146', '2023-05-20 10:50:26'),
+(3, 'mrifki@gmail.com', 'M Rifki', '0142321222184', '601f1889667efaebb33b8c12572835da3f027f78', '61017', '2023-05-20 11:46:53'),
+(4, 'muhrifki@gmail.com', 'M Rifki Abdillah', '0202123121', '601f1889667efaebb33b8c12572835da3f027f78', '44855', '2023-06-13 01:45:42');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_catches`
+--
+ALTER TABLE `tbl_catches`
+  ADD PRIMARY KEY (`catch_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `tbl_users`
@@ -61,10 +101,26 @@ ALTER TABLE `tbl_users`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_catches`
+--
+ALTER TABLE `tbl_catches`
+  MODIFY `catch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_catches`
+--
+ALTER TABLE `tbl_catches`
+  ADD CONSTRAINT `tbl_catches_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
